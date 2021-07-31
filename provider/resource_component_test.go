9b0dca2de1d9c8cd253fdc78b6a66ad4e5689ac5
mories76 <m.kok@mories.com>
2021-07-31 16:22:30 +0200

    more testing

--- provider/resource_component_test.go
+++ provider/resource_component_test.go
@@ -9,7 +9,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccResourceComponent(t *testing.T) {
+func TestAccResourceComponent_basic(t *testing.T) {
+	t.Parallel()
 
 	RootWorkspace := os.Getenv("ARDOQ_WORKSPACE")
 
@@ -24,25 +25,95 @@ func TestAccResourceComponent(t *testing.T) {
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceComponent(RootWorkspace, componentName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.ardoq_component.my-component", "name", componentName),
-				),
+				Config: testAccResourceComponent_basic(RootWorkspace, componentName),
+			},
+			{
+				ResourceName:      "ardoq_component.my-component",
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
 }
 
-func testAccResourceComponent(RootWorkspace, componentName string) string {
+func TestAccResourceComponent_full(t *testing.T) {
+	t.Parallel()
+
+	RootWorkspace := os.Getenv("ARDOQ_WORKSPACE")
+
+	if RootWorkspace == "" {
+		t.Skip("ARDOQ_WORKSPACE needs to be set to run this test")
+	}
+
+	componentName := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceComponent_full(RootWorkspace, componentName),
+			},
+			{
+				ResourceName:      "ardoq_component.my-component",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccResourceComponent_fullUpdate(RootWorkspace, componentName),
+			},
+			{
+				ResourceName:      "ardoq_component.my-component",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccResourceComponent_basic(RootWorkspace, componentName string) string {
 	return fmt.Sprintf(`
 resource "ardoq_component" "my-component" {
   root_workspace = "%s"
   name = "%s"
   description = "TestAcc"
 }
-data "ardoq_component" "my-component" {
-  root_workspace = ardoq_component.my-component.root_workspace
-  name = ardoq_component.my-component.name
+`, RootWorkspace, componentName)
 }
+
+func testAccResourceComponent_full(RootWorkspace, componentName string) string {
+	return fmt.Sprintf(`
+resource "ardoq_component" "my-parent" {
+	root_workspace = "%[1]s"
+	name = "%[2]s-parent"
+	description = "TestAcc"
+	}
+resource "ardoq_component" "my-component" {
+  root_workspace = "%[1]s"
+  name = "%[2]s"
+  description = "TestAcc"
+  parent = ardoq_component.my-parent.id
+  fields = {
+	  "bewaartermijn" = "heel lang"
+  }
+}
+`, RootWorkspace, componentName)
+}
+
+func testAccResourceComponent_fullUpdate(RootWorkspace, componentName string) string {
+	return fmt.Sprintf(`
+	resource "ardoq_component" "my-parent" {
+		root_workspace = "%[1]s"
+		name = "%[2]s-parent"
+		description = "TestAcc"
+		}
+	resource "ardoq_component" "my-component" {
+	  root_workspace = "%[1]s"
+	  name = "%[2]s"
+	  description = "TestAcc updated"
+	  fields = {
+		"bewaartermijn" = "heel lang updated"
+	}
+	}
 `, RootWorkspace, componentName)
 }
