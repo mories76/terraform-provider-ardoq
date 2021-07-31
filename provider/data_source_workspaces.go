9cfb75782aea37121f34726144e24de682e9fd65
mories76 <m.kok@mories.com>
2021-07-31 16:24:57 +0200

    refactor data_source workspaces

--- provider/data_source_workspaces.go
+++ provider/data_source_workspaces.go
@@ -10,45 +10,47 @@ import (
 	ardoq "github.com/mories76/ardoq-client-go/client"
 )
 
+var componentSchema = map[string]*schema.Schema{
+	"id": &schema.Schema{
+		Description: "The unique ID of the workspace",
+		Type:        schema.TypeString,
+		Computed:    true,
+	},
+	"name": &schema.Schema{
+		Description: "Name of workspace",
+		Type:        schema.TypeString,
+		Required:    true,
+	},
+	"component_model": &schema.Schema{
+		Description: "Id of the model the workspace is based on",
+		Type:        schema.TypeString,
+		Computed:    true,
+	},
+	"component_template": &schema.Schema{
+		Description: "Id of the template the workspace is based on",
+		Type:        schema.TypeString,
+		Computed:    true,
+	},
+	"description": &schema.Schema{
+		Description: "Text field describing the workspace",
+		Type:        schema.TypeString,
+		Computed:    true,
+	},
+	"fields": {
+		Description: "All custom fields from the model end up here",
+		Type:        schema.TypeMap,
+		Computed:    true,
+		Elem: &schema.Schema{
+			Type: schema.TypeString,
+		},
+	},
+}
+
 func dataSourceArdoqWorkspace() *schema.Resource {
 	return &schema.Resource{
 		Description: "`arodq_workspace` data source returns a workspace",
 		ReadContext: dataSourceWorkspaceRead,
-		Schema: map[string]*schema.Schema{
-			"id": &schema.Schema{
-				Description: "The unique ID of the workspace",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"name": &schema.Schema{
-				Description: "Name of workspace",
-				Type:        schema.TypeString,
-				Required:    true,
-			},
-			"component_model": &schema.Schema{
-				Description: "Id of the model the workspace is based on",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"component_template": &schema.Schema{
-				Description: "Id of the template the workspace is based on",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"description": &schema.Schema{
-				Description: "Text field describing the workspace",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"fields": {
-				Description: "All custom fields from the model end up here",
-				Type:        schema.TypeMap,
-				Computed:    true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
-			},
-		},
+		Schema:      componentSchema,
 	}
 }
 
@@ -62,41 +64,7 @@ func dataSourceArdoqWorkspaces() *schema.Resource {
 				Type:        schema.TypeList,
 				Computed:    true,
 				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"id": &schema.Schema{
-							Description: "The unique ID of the workspace",
-							Type:        schema.TypeString,
-							Computed:    true,
-						},
-						"name": &schema.Schema{
-							Description: "Name of workspace",
-							Type:        schema.TypeString,
-							Computed:    true,
-						},
-						"component_model": &schema.Schema{
-							Description: "Id of the model the workspace is based on",
-							Type:        schema.TypeString,
-							Computed:    true,
-						},
-						"component_template": &schema.Schema{
-							Description: "Id of the template the workspace is based on",
-							Type:        schema.TypeString,
-							Computed:    true,
-						},
-						"description": &schema.Schema{
-							Description: "Text field describing the workspace",
-							Type:        schema.TypeString,
-							Computed:    true,
-						},
-						"fields": {
-							Description: "All custom fields from the model end up here",
-							Type:        schema.TypeMap,
-							Computed:    true,
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
-						},
-					},
+					Schema: componentSchema,
 				},
 			},
 		},
